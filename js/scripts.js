ba86990a5f5e4becb19184993bbde405df364e44
Yelena Nedelko & Kendra Ash <student@epicodus.com>
2015-09-10 10:26:16 -0700

    return the output for counting to upTo by countBy and add a web view.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
 var numberCounter = function(upTo, countBy) {
+
   if ((upTo && countBy != null) && (upTo >= countBy)) {
-    return true;
+    return countUpBy(upTo, countBy);
   }
   else {
-    return false;
+    return "please provide valid values";
   }
 };
+
+var countUpBy = function(upTo, countBy) {
+  var counter = 0;
+  var output = [];
+  while(counter < upTo) {
+    counter = counter + countBy;
+    if (counter > upTo)
+      break;
+    output.push(counter);
+  }
+  return output;
+};
+
+
+$(document).ready(function() {
+  $("form#count-up-by").submit(function(event) {
+    var upTo = parseInt($('input#upTo').val());
+    var countBy = parseInt($('input#countBy').val());
+    var output = numberCounter(upTo, countBy);
+
+    $(".upTo").text(upTo);
+    $(".countBy").text(countBy);
+    $(".output").text(output);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,11 @@
 describe("numberCounter", function() {
   it("returns false if only one input is entered", function() {
-    expect(numberCounter(10)).to.equal(false)
+    expect(numberCounter(10)).to.equal("please provide valid values")
   });
   it("returns false if upTo is less than countBy", function() {
-    expect(numberCounter(1,5)).to.equal(false)
+    expect(numberCounter(1,5)).to.equal("please provide valid values")
+  });
+  it("returns the output for counting to upTo by countBy", function() {
+    expect(numberCounter(10,2)).to.eql([2,4,6,8,10])
   });
 });
